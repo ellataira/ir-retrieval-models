661e31f493ddbdac46a4852213d6e4af86a04ec8
ellataira <ejtaira177@gmail.com>
2023-01-25 14:11:22 -0500

    fixing up

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,6 @@ import os
 import re
 
 from elasticsearch7 import Elasticsearch
-# from elasticsearch.client import IndicesClient
 
 data = "/Users/ellataira/Desktop/cs4200/homework-1-ellataira/IR_data /AP_DATA/ap89_collection"
 
@@ -23,7 +22,8 @@ def main() :
 
     es = Elasticsearch("http://localhost:9200")
 
-    es.info()
+    # delete previously made index if it already exists
+    es.indices.delete(index=AP89_INDEX, ignore=[404, 400])
 
     request_body={
         "settings": {
@@ -52,11 +52,13 @@ def main() :
     response = es.indices.create(index=AP89_INDEX, body=request_body)
     print(response)
 
-    open(es)
+    open_dir(es)
+
+    print("completed indexing!")
 
 
 """opens file collection and delegates to parse individual files """
-def open(es) :
+def open_dir(es) :
 
     entries = os.listdir(data) ##TODO FIX THISJKDKLFJKLAJDFKL
     id = 0
@@ -64,36 +66,33 @@ def open(es) :
     # for every 'ap....' file in the opened directory, parse it for documents
     for entry in entries:
         if 'ap' in entry: ## excludes the readme file
-            filepath = './ap89_collection/' + entry
+            filepath = data + "/" + entry
             id = parse(filepath, id, es)
 
-            print(filepath+ "\n")
 
 
 """parses an individual file from the collection for documents / info """
 def parse(filepath, id, es):
 
-    with open(filepath) as opened:
-        found_docs = re.findall(DOC_REGEX, opened)
+    with open(filepath, encoding="ISO-8859-1") as opened:
+
+        read_opened = opened.read()
+        found_docs = re.findall(DOC_REGEX, read_opened)
 
         for doc in found_docs:
             id +=1
-            print(id + "\n")
 
             docno = ""
             text = ""
 
-
             find_docno = re.search(DOCNO_REGEX, doc)
             if find_docno:
-                docno = re.sub(DOCNO_REGEX, "", find_docno)  ##TODO: getting warning bc re.search returns String or None, and re.sub doesnt support None
+                docno = re.sub(DOCNO_REGEX, "", str(find_docno))  ##TODO: getting warning bc re.search returns String or None, and re.sub doesnt support None
                 docno.strip()
-                print(docno+ "\n")
 
             find_text = re.search(TEXT_REGEX, doc)
             if find_text:
-                text = re.sub(TEXT_REGEX, "", find_text)
-                print(text+ "\n")
+                text = re.sub(TEXT_REGEX, "", str(find_text))
 
             parsed_doc =  {
                 'DOCNO': docno,
